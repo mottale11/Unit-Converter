83175f3d17170a1857e692b10915baf7592d77cf
Moses Mwai <148555858+mottale11@users.noreply.github.com>
2024-11-19 07:43:18 +0300

    script.js

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ else {result = inputValue / 1.60934}
 const resultString = inputValue + " miles are " + result + "km";
 console.log(resultString);
 const resultElement = document.getElementById("resultElement");
+resultElement.innerHTML = resultString;
