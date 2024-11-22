4534b4a70752755e4163f6c19c34d44b7c9ade80
Moses Mwai <148555858+mottale11@users.noreply.github.com>
2024-11-22 08:39:51 +0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// input value
-let inputValue = 10;
-let mileToKm = false;
-let result = 0;
-if (milesToKm) {result = inputValue * 1.60934;}
-else {result = inputValue / 1.60934}
-const resultString = inputValue + " miles are " + result + "km";
-console.log(resultString);
-const resultElement = document.getElementById("resultElement");
-resultElement.innerHTML = resultString;
+function convert() {
+  const inputValue = document.getElementById("userInput").value;
+  const unit = document.getElementById("unit").value;
+  let milesToKm = unit === "milesToKm";
+  let result = 0;
+  if (milesToKm) {result = inputValue * 1.60934;}
+  else {result = inputValue / 1.60934;}
+  const resultString = inputValue + " miles are " + result + "km";
+  console.log(resultString);
+  const resultElement = document.getElementById("resultElement");
+  resultElement.innerHTML = resultString;
+}
